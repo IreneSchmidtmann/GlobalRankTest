6670fcdbdfa05679269b9d6309dc3643708bc2e1
Irene Schmidtmann <Irene.Schmidtmann@uni-mainz.de>
2017-03-20 12:32:43 +0100

    Added (local) functions to evaluate pi_x1, pi_x2, pi_x3

--- Moment function.R
+++ Moment function.R
@@ -1,4 +1,4 @@
-moments <- function(p_0 = 0.2, p_1 = 0.2, mu_0 = 1, mu_1 = 0.05, sigma = 1, tau=1){
+moments <- function(p_0 = 0.2, p_1 = 0.2, mu_0 = 1, mu_1 = 1, sigma = 1, tau=1){
   if (p_0 > 0 && p_1 > 0) {
     q_0 <- 1 - p_0
     q_1 <- 1 - p_1
@@ -13,21 +13,39 @@ moments <- function(p_0 = 0.2, p_1 = 0.2, mu_0 = 1, mu_1 = 0.05, sigma = 1, tau=
     print(paste0("pi_t3 = ", pi_t3))
   }
   delta <- mu_1 - mu_0
+  print(delta)
+  
+  shift <- delta / sigma
+  
+  integrand1 <- function(x) {
+    return(dnorm(x + shift)*pnorm(x))
+  }  
+  integrand2 <- function(x) {
+    return(dnorm(x + shift)*(pnorm(x))^2)
+  }  
+
+  integrand3 <- function(x) {
+    return(dnorm(x)*(pnorm(x + shift))^2)
+  }  
+
+  pi_x1 <- integrate(integrand1, -Inf, Inf)$value
+  print(paste0("pi_x1 = ", pi_x1))
+  
+  pi_x2 <- integrate(integrand2, -Inf, Inf)$value
+  print(paste0("pi_x2 = ", pi_x2))
+
+  pi_x3 <- integrate(integrand3, -Inf, Inf)$value
+  print(paste0("pi_x3 = ", pi_x3))
+  
   mu_u <- mu_0 + mu_1
   sigma_u <- 2 * sigma
   list(mu_u, sigma_u)
   return(list(mean_u = mu_u, sigma_u = sigma_u))
 }
 
-mymoments <- moments(p_0 = 0, p_1 = 0, mu_0 = 0, mu_1 = 0.05, sigma = 1, tau = 1)
+mymoments <- moments(p_0 = 0, p_1 = 0, mu_0 = 0.3, mu_1 = 0.25, sigma = 1, tau = 1)
 print(mymoments)
 
-integrate(dnorm, -1.96, 1.96)
-integrate(dnorm, -Inf, Inf)
 
-shift <- 0.2
-integrand1 <- function(x) {
-  return(dnorm(x + shift)*pnorm(x))
-}
 
-integrate(integrand1, -Inf, Inf)
+
