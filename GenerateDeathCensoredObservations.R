4644dbcd57c3406c785a0a733b2362c99207cd6f
IreneSchmidtmann <ischmidt@uni-mainz.de>
2018-04-07 19:49:10 +0200

    Modified function generate_dco - (removed bug) - added simulation loops

--- GenerateDeathCensoredObservations.R
+++ GenerateDeathCensoredObservations.R
@@ -54,23 +54,38 @@ generate_dco <- function(n_0 = 50, n_1 = 50,
 # $a_1 = $ scale parameter in treatment group, derived from p_1
   q_0 <- 1 - p_0
   q_1 <- 1 - p_1
-  a_0 <- tau * (q_0 / p_0)^(1/b_0)
-  a_1 <- tau * (q_1 / p_1)^(1/b_1)
-  t_0 <- rllogis(n_0, shape = b_0, scale = a_0)
-  t_1 <- rllogis(n_1, shape = b_1, scale = a_1)
-# generate death indicators
-  d_0 <- ifelse(t_0 <= tau, 1, 0)
-  d_1 <- ifelse(t_1 <= tau, 1, 0)
-# generate quantiative endpoints
+  t_0 <- rep(NA, n_0)
+  t_1 <- rep(NA, n_1)
+  if (p_0 > 0) {
+    # determine scale parameter
+    a_0 <- tau * (q_0 / p_0)^(1/b_0)
+    # generate time to death variable
+    t_0 <- rllogis(n_0, shape = b_0, scale = a_0)
+    # generate death indicators
+    d_0 <- ifelse(t_0 <= tau, 1, 0)
+  }else{
+    d_0 <- rep(0, n_0)
+  }
+  if (p_1 > 0) {
+    # determine scale parameter
+    a_1 <- tau * (q_1 / p_1)^(1/b_1)
+    # generate time to death variable
+    t_1 <- rllogis(n_1, shape = b_1, scale = a_1)
+    # generate death indicators
+    d_1 <- ifelse(t_1 <= tau, 1, 0)
+  }else{
+    d_1 <- rep(0, n_1)
+  }
+  # generate quantiative endpoints
   x_0 <- rnorm(n_0, mean = mu_0, sd = sigma)
   x_1 <- rnorm(n_1, mean = mu_1, sd = sigma)
   eta <- min(c(x_0, x_1)) - 1 - tau
   if (!tied) {
-    xx_0 <- ifelse(t_0 <= tau, eta + t_0, x_0)
-    xx_1 <- ifelse(t_1 <= tau, eta + t_0, x_1)
+    xx_0 <- ifelse(d_0 == 1, eta + t_0, x_0)
+    xx_1 <- ifelse(d_1 == 1, eta + t_1, x_1)
   }else {
-    xx_0 <- ifelse(t_0 <= tau, eta, x_0)
-    xx_1 <- ifelse(t_1 <= tau, eta, x_1)
+    xx_0 <- ifelse(d_0 == 1, eta, x_0)
+    xx_1 <- ifelse(d_1 == 1, eta, x_1)
   }
   if (verbose) {
     print(paste0("t_0 = ", round(t_0, digits = 3)))
@@ -90,25 +105,43 @@ generate_dco <- function(n_0 = 50, n_1 = 50,
 }
 
 # set seed for random number generation for reproducibility
-set.seed(2703108)
-n_0 <- 50
-n_1 <- 50
-results_gen_dco <- generate_dco(n_0 = n_0, n_1 = n_1, tau = 1)
-
-wilcoxon_result <- wilcox.test(results_gen_dco$xx_0, results_gen_dco$xx_0)
-mw <- wilcoxon_result$statistic / (n_0*n_1)
-
-p_0_0 <- 0.0
-p_1_0 <- 0.0
+set.seed(270318)
+no_scenarios <- 3
+n_0 <- c(49, 51, 54)
+n_1 <- 2 * n_0
+p_0 <- c(0.00, 0.01, 0.02)
+RR <- c(1, 1, 1)
+p_1 <- RR * p_0
 tied <- FALSE
-mu_0_0 <- 0.25
-mu_1_0 <- 0.3
+mu_0_0 <- 0.3
+mu_1_0 <- 0.25
+mu_0_1 <- 0.3
+mu_1_1 <- 0.3
 sigma <- 0.1
 tau <- 1
 verbose <- FALSE
 
-moments_H_0 <- moments(p_0 = p_0_0, p_1 = p_1_0, mu_0 = mu_0_0, mu_1 = mu_1_0,
+r <- 10
+wx <- matrix(nrow = r, ncol = no_scenarios)
+mw <- matrix(nrow = r, ncol = no_scenarios)
+for (sc in (1:no_scenarios)) {
+  for (i in seq(1:r)) {
+    results_gen_dco <- generate_dco(n_0 = n_0, n_1 = n_1,
+                                    p_0 = p_0, p_1 = p_1,
+                                    mu_0 = mu_0_1, mu_1 = mu_1_1,
+                                    sigma = sigma,
+                                    tau = 1,
+                                    b_0 = 1, b_1 = 1,
+                                    verbose = verbose, tied = FALSE)
+    wx[i, sc] <- wilcox.test(results_gen_dco$xx_1, results_gen_dco$xx_0)$statistic
+  }
+}
+
+mw <- wx / (n_0 * n_1)
+
+moments_H_0 <- moments(p_0 = p_0, p_1 = p_1, mu_0 = mu_0_0, mu_1 = mu_1_0,
                        sigma = sigma, tau = tau, n_0 = n_0, n_1 = n_1,
                        verbose = verbose, tied = tied)
-moments_H_0$mu_u
-moments_H_0$sigma_u
+mw_standardized <- (mw - moments_H_0$mu_u) / moments_H_0$sigma_u
+p_values <- pnorm(mw_standardized, lower.tail = FALSE)
+table(p_values <= 0.025)
